35d5ee0100a86b6a59661f33a2c5f697fc97da0e
Pritha Mishra <prithamishra1@gmail.com>
2018-02-18 13:55:38 -0700

    final commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+//Letters to chose from
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//declaring and initializing the variables Setting all to zero
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesLeft = 10;
+var guessedLetters = [];
+var letterToGuess = null;
+
+//computer selects a random letter
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//Updates user's guesses and sets them to the most recent
+var setGuessesLeft = function() {
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var setLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var setGuessesSoFar = function() {
+  document.querySelector('#guessedLetters').innerHTML = "You have guessed these letters: " + guessedLetters.join(', ');
+};
+// Reset function 
+var reset = function() {
+  totalGuesses = 10;
+  guessesLeft = 10;
+  guessedLetters = [];
+
+  setLetterToGuess();
+  setGuessesLeft();
+  setGuessesSoFar();
+}
+
+//user's guess is saved on keyup
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  setGuessesLeft();
+  setGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("Yes, you are psychic!");
+                reset();
+            }
+        }else if(guessesLeft == 0){
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("Sorry, you're not psychic, try again!!");
+
+            reset();
+        }
+
+
+};
